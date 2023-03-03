727fde0d3e065b6d360149c9398a4fdfb36883ad
Renan Nunes <renan_nunesdasilva12@hotmail.com>
2023-03-03 20:19:34 -0300

    Testes aulas

--- src/tiposprimitivos.java
+++ src/tiposprimitivos.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 public class tiposprimitivos {
     public static void main(String[] args){
+        /* *
         Scanner teclado = new Scanner(System.in);
         Scanner teclado1 = new Scanner(System.in);
         System.out.print("Qual o nome do Aluno?");
@@ -8,5 +9,15 @@ public class tiposprimitivos {
         System.out.print("Qual a nota do aluno?");
         float nota = teclado1.nextFloat();
         System.out.format("A sua nota é "+ nota +" "+ nome);
+        */
+
+       /* * int idade = 23;
+        String valor = Integer.toString(idade);
+        System.out.println(valor);
+        String valor = "23";
+        int Idade = Integer.parseInt(valor);
+        System.out.println(Idade);*/
+        
+
     }
 }
