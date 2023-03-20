ad41e9bd9f771581f1f3910f84fb415ace00c608
Renan Nunes <renan_nunesdasilva12@hotmail.com>
2023-03-20 19:18:57 -0300

    2.0 interface

--- src/Interface.java
+++ src/Interface.java
@@ -2,6 +2,9 @@ import javax.swing.JOptionPane;
 
 public class Interface {
     public static void main(String[] args) {
-       String name = JOptionPane.showInputDialog("Qual o seu nome?");
+       String nome = JOptionPane.showInputDialog("Qual o seu nome?");
+       int idade = Integer.parseInt(JOptionPane.showInputDialog("Qual a sua Idade? "));
+       JOptionPane.showMessageDialog(null,"Olá " +nome+" Você têm "+idade+" anos");
+       
     }   
 }
