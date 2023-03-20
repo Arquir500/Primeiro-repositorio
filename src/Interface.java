f7a4a7d6c12b8d9a6aacb6991d75c8701f46410b
Renan Nunes <renan_nunesdasilva12@hotmail.com>
2023-03-20 19:10:08 -0300

    Interface

---
+++ src/Interface.java
@@ -0,0 +1,7 @@
+import javax.swing.JOptionPane;
+
+public class Interface {
+    public static void main(String[] args) {
+       String name = JOptionPane.showInputDialog("Qual o seu nome?");
+    }   
+}
