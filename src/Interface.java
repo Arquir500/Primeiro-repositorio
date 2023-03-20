befea9f02cc34a6c09372319fc7dad888535a9b2
Renan Nunes <renan_nunesdasilva12@hotmail.com>
2023-03-20 19:28:03 -0300

    atualizaçãoInterface2,0

--- src/Interface.java
+++ src/Interface.java
@@ -4,7 +4,10 @@ public class Interface {
     public static void main(String[] args) {
        String nome = JOptionPane.showInputDialog("Qual o seu nome?");
        int idade = Integer.parseInt(JOptionPane.showInputDialog("Qual a sua Idade? "));
-       JOptionPane.showMessageDialog(null,"Olá " +nome+" Você têm "+idade+" anos");
+       double peso = Double.parseDouble(JOptionPane.showInputDialog("Qual o seu peso? "));
+       double altura = Double.parseDouble(JOptionPane.showInputDialog("Qual sua altura?"));
+       String sexo = JOptionPane.showInputDialog("Qual seu Gênero?");
+       JOptionPane.showMessageDialog(null,"Olá " +nome+" Você têm "+idade+" anos! você é "+sexo+" têm"+altura+" de altura é"+peso+"kilos");
        
     }   
 }
