4515de57175419203ec268d075cb9e17359ca5a0
Renan Nunes <renan_nunesdasilva12@hotmail.com>
2023-03-20 22:14:20 -0300

    Randomicos1.0

--- src/randomicos.java
+++ src/randomicos.java
@@ -1,3 +1,12 @@
+import java.util.Random;
 public class randomicos {
-    
+    public static void main(String[] args){
+        Random random = new Random();
+        int x = random.nextInt(6)+1;
+        double y = random.nextDouble(6)+1;
+        boolean z = random.nextBoolean();
+        System.out.print(x+" ");
+        System.out.print(" "+y+" ");
+        System.out.print(" "+z);
+    }
 }
